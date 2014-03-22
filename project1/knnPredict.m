8de41a2664a7f6ab23ab52a41819259ee6e198f3
Vinod Galla <vgalla@buffalo.edu>
2014-03-22 10:43:57 -0400

    Update knnPredict.m

--- project1/knnPredict.m
+++ project1/knnPredict.m
@@ -1,36 +1,67 @@
 function label = knnPredict(k, train_data, train_label, test_data)
 [train_rows,train_columns] = size(train_data);
 [train_label_rows,train_label_columns] = size(train_label);
-[test_data_rows,test_data_columns] = size(test_data);
+[test_rows,test_columns] = size(test_data);
 
 
-if (train_label_columns > 1)
-    Group = zeros(train_label_rows,1);
-    for i = 1:1:train_label_rows
-        [maxValue, maxIndex] = max(train_label(i,:),[],2);
-        Group(i,1) = maxIndex;
-    end
-else
-    Group = train_label;
-end
+%if (train_label_columns > 1)
+%    Group = zeros(train_label_rows,1);
+%    for i = 1:1:train_label_rows
+%        [maxValue, maxIndex] = max(train_label(i,:),[],2);
+%        Group(i,1) = maxIndex;
+%    end
+%else
+
+%end
 
 %Sample = test_data;
 %Training = train_data;
-
+%Group = train_label;
 
 %Class = knnclassify(Sample, Training, Group)
 %Class = knnclassify(Sample, Training, Group, k)
 %Class = knnclassify(Sample, Training, Group, k, distance)
 %Class = knnclassify(Sample, Training, Group, k, distance, rule)
-
-single_column_label = knnclassify(test_data,train_data,Group,k);
-
-int_column_label = int32(single_column_label);
-
-label = zeros(test_data_rows,train_label_columns);
-
-for i = 1:1:test_data_rows
-    label(i,int_column_label(i,1)) = 1;
+label = zeros(test_rows,1);            
+if (train_label_columns == 1)
+    if (train_rows == train_label_rows)
+        if (train_columns == test_columns)
+            label = knnclassify(test_data,train_data,train_label,k);
+            
+%
+%           CODE WITHOUT USING KNNCLASSIFT() 
+%
+%             [gIndex,groups] = grp2idx(train_label);
+%             for i = 1:1:test_rows
+%                 column_dists_each_test = zeros(train_rows,1);
+%                 for j = 1:1:train_rows
+%                     dist = sum((train_data(j,:) - test_data(i,:)).^2,2);
+%                     %column_dists_each_test = sum((train_data - test_data(repmat(i,train_rows,1),:)).^2, 2);
+%                     column_dists_each_test(j,1) = dist;
+%                 end
+%                 [dSorted,dIndex] = sort(column_dists_each_test);
+%                 dIndex = dIndex(1:k,1);
+%                 possible_classes = gIndex(dIndex);
+%                 label(i,1) = mode(possible_classes);
+%             end
+%             if isnumeric(train_label) || islogical(train_label)
+%                 groups = str2num(char(groups)); %#ok
+%                 label = groups(label);
+%             elseif ischar(train_label)
+%                 groups = char(groups);
+%                 label = groups(label,:);
+%             else %if iscellstr(group)
+%                 label = groups(label);
+%             end            
+%            
+        else
+            sprintf('Training data and Test data feature count not matching!!');
+        end
+    else
+        sprintf('Training data and Training label have different number of rows!!');
+    end
+else
+    sprintf('Training label should be a single column matrix!!');
 end
 
 end
