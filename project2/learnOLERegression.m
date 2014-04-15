fab2bb235eaceb56124f80349a1320b36840149c
Bharadwaj Avva <vv.bharadwaj@gmail.com>
2014-04-15 12:23:39 -0400

    Q1 and Q2 answers

--- project2/learnOLERegression.m
+++ project2/learnOLERegression.m
@@ -6,3 +6,27 @@ function w = learnOLERegression(X,y)
 % y = N x 1
 % Output:
 % w = D x 1
+
+
+[N D] = size(X);
+[y_rows y_columns] = size(y);
+
+w = zeros(N,1);
+
+if (not(y_rows == N))
+    sprintf('X and y should have the same number of rows!!');
+else
+    if (not(y_columns == 1))
+        sprintf('y should be a vector!!');
+    else
+        
+        X_transpose = transpose(X);
+        
+        % w_MLE = inverse(transpose(X) * X) * transpose(X) * y;
+        
+        w = mtimes(inv(mtimes(X_transpose,X)) , mtimes(X_transpose,y));
+        
+    end
+end
+
+end
--- project2/learnRidgeRegression.m
+++ project2/learnRidgeRegression.m
@@ -7,3 +7,37 @@ function w = learnRidgeRegression(X,y,lambda)
 % lambda = scalar
 % Output:
 % w = D x 1
+
+% Extracting dimensions from inputs
+
+[N D] = size(X);
+
+w = zeros(D:1);
+
+[y_rows y_cols] = size(y);
+
+if (not(y_rows == N))
+    sprintf('Training data and label should have the same number of rows!!');
+else
+    if (not(y_cols == 1))
+        sprintf('Training label should a N*1 column matrix!!');
+    else      
+        
+        % I_D = eye(D+1);
+        X_transpose = transpose(X);
+
+        % w_ridge = inverse(lambda*N*I_D + transpose_X*X)*transpose_X*y;
+        
+        w = mtimes(inv(lambda*N*eye(D) + mtimes(X_transpose,X)),mtimes(X_transpose,y));
+    end
+end
+
+
+
+
+
+
+
+w = mtimes(mtimes(inv(+),transpose(X)),y);
+
+end
