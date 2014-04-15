8aa4230e14607b67b077dd6d4da9f9272616dca7
Vinod Galla <gallavinod@users.noreply.github.com>
2014-04-15 15:27:03 -0400

    Update mapNonLinear.m

--- project2/mapNonLinear.m
+++ project2/mapNonLinear.m
@@ -4,4 +4,11 @@ function x_n = mapNonLinear(x,d)
 % d - integer (>= 0)
 % Outputs:
 % x_n - (N x (d+1))
+
+x_original = x;
+x_n = ones(size(x,1),d+1);
+for i=1:1:d
+    x_n(:,i+1) = x_original.^i;
+end
+
 end
