f8c3945d686665e99f7028dadde8d1e971ecae4a
Bharadwaj Avva <vv.bharadwaj@gmail.com>
2014-04-15 14:41:35 -0400

    mistake with N and lambda

--- project2/regressionObjVal.m
+++ project2/regressionObjVal.m
@@ -2,6 +2,6 @@ function [error, error_grad] = regressionObjVal(w, X, y, lambda)
 
 N = size(X, 1);
 error = ((sum((y' - (w' * X')) .* (y' - (w' * X')))) ./ N) + (lambda .* (w' * w));
-error_grad = (lambda .* w - (X' * (y - (X * w)))) .* 2 ./ lambda;
+error_grad = (lambda .* w - (X' * (y - (X * w)))) .* 2 ./ N;
 
 end
